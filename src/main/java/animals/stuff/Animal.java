84c6bce8223af19137f56b6d269146da03d9fcf8
SpaceTilmar <21tilmaro@gmail.com>
2023-09-13 17:31:46 -0400

    Added Main Files

---
+++ src/main/java/animals/stuff/Animal.java
@@ -0,0 +1,28 @@
+package animals.stuff;
+
+public class Animal {
+
+    public String animalName[];
+    public int animalCount;
+    public boolean animalAlive;
+    public String sound;
+
+    public Animal(int animalCount, boolean animalAlive, String animalName[], String sound) {
+        this.animalCount = animalCount;
+        this.animalName = animalName;
+        this.animalAlive = animalAlive;
+        this.sound = sound;
+
+    }
+    public void printAnimalSound() {
+        System.out.println(sound);
+    }
+    public void printAnimalStatus() {
+        for (int i = 0; i < animalName.length; i++){
+        System.out.println(animalName[i]);
+        }
+        System.out.println(animalCount);
+        System.out.println(animalAlive);
+    }
+
+}
---
+++ src/main/java/animals/stuff/Main.java
@@ -0,0 +1,27 @@
+package animals.stuff;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        String zebraNames[] = {"Brad", "Chad", "Tyler", "Kevin"};
+        String platypusNames[] = {"Perry" , "Also Perry" , "A Normal Platypus(Not Perry)"};
+        String dogNames[] = {"Tux"};
+        Animal zebra = new Animal(4, true, zebraNames, "Zebra Noises");
+        Animal platypus = new Animal(3, true, platypusNames, "Grrr");
+        Animal dog = new Animal(1, true, dogNames, "Bark bark bark");
+        dog.printAnimalSound();
+        System.out.println("++++++++++");
+        dog.printAnimalStatus();
+        System.out.println("----------");
+        platypus.printAnimalSound();
+        System.out.println("++++++++++");
+        platypus.printAnimalStatus();
+        System.out.println("----------");
+        zebra.printAnimalSound();
+        System.out.println("++++++++++");
+        zebra.printAnimalStatus();
+        System.out.println("----------");
+    }
+}
